328651dc10386b8b60a313b877cfa278daf4252d
hanzhichen <hanzhichen@tracyenergy.cn>
2026-05-22 17:11:13 +0800

    feat: add graceful api shutdown

--- apps/api/cmd/server/main.go
+++ apps/api/cmd/server/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
+	"log/slog"
 	stdhttp "net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/hanzc0106/commune/apps/api/internal/app"
@@ -13,13 +17,15 @@ import (
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	cfg := config.Load()
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	pool, err := db.Open(ctx, cfg.DatabaseURL)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error("database_open_failed", "error", err)
+		os.Exit(1)
 	}
 	defer pool.Close()
 
@@ -29,8 +35,50 @@ func main() {
 		APIHandler: apphttp.NewAPI(service),
 	})
 
-	log.Printf("commune api listening on %s", cfg.HTTPAddr)
-	if err := stdhttp.ListenAndServe(cfg.HTTPAddr, handler); err != nil {
-		log.Fatal(err)
+	runCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	server := &stdhttp.Server{
+		Addr:              cfg.HTTPAddr,
+		Handler:           handler,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
+
+	if err := serveHTTP(runCtx, server, logger, 10*time.Second); err != nil {
+		logger.Error("server_failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func serveHTTP(ctx context.Context, server *stdhttp.Server, logger *slog.Logger, shutdownTimeout time.Duration) error {
+	errCh := make(chan error, 1)
+	go func() {
+		logger.Info("server_starting", "addr", server.Addr)
+		errCh <- server.ListenAndServe()
+	}()
+
+	select {
+	case err := <-errCh:
+		if errors.Is(err, stdhttp.ErrServerClosed) {
+			logger.Info("server_stopped", "addr", server.Addr)
+			return nil
+		}
+		return err
+	case <-ctx.Done():
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		return err
+	}
+
+	err := <-errCh
+	if err != nil && !errors.Is(err, stdhttp.ErrServerClosed) {
+		return err
+	}
+
+	logger.Info("server_stopped", "addr", server.Addr)
+	return nil
 }
